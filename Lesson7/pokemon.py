f1494c13a0456763a2445ee60a80dbb57418b3f9
Paul Walter <hidden@email.secret>
2024-02-05 14:27:11 +0000

    pokemon api example

---
+++ Lesson7/pokemon.py
@@ -0,0 +1,50 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+import requests
+
+def fetch_pokemon_data(pokemon_name):
+    url = f"https://api.pokemontcg.io/v2/cards?q=name:{pokemon_name}"
+    response = requests.get(url)
+    return response.json()
+
+
+class Application(App):
+
+    def __init__(self):
+        super().__init__()
+        self.pokemonData = {}
+
+    def update_label(self):
+        for pokemon in self.pokemonData:
+            self.pokemonDataLabel.text += f"{pokemon['name']}: {pokemon['hp']}\n"
+
+    def build(self):
+        layout = BoxLayout(orientation="vertical")
+        self.pokemonNameInput = TextInput(multiline=False)
+        layout.add_widget(self.pokemonNameInput)
+        
+        self.fetchButton = Button(text="Fetch Pokemon Data", on_click=self.handle_click)
+        layout.add_widget(self.fetchButton)
+
+        self.pokemonDataLabel = Label(text="")
+        layout.add_widget(self.pokemonDataLabel)
+
+        return layout
+
+
+    def handle_click(self, button):
+        if button.text != 'Fetch Pokemon Data': return
+        self.pokemonData = fetch_pokemon_data(self.pokemonNameInput.text)
+        print('Fetched data: ', self.pokemonData)
+        # self.update_label()
+
+
+if __name__ == "__main__":
+    myApp = Application()
+    myApp.run()
