7a02e44202a5fdba65d2aa96db83c48071f94b1e
Paul Walter <hidden@email.secret>
2024-03-04 14:42:49 +0000

    Removed unused imports and added explanations in comments

--- Lesson8/screens.py
+++ Lesson8/screens.py
@@ -1,12 +1,14 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.properties import NumericProperty, StringProperty
+from kivy.properties import StringProperty
 
 from random import choice
 
 
-# Declare both screens
+# The screens are almost the same - and normally we would use inheritance here.
+# I write them separately to show they can be completely different, as long as they
+#   both inherit from Screen
 class FirstPlayerScreen(Screen):
     results = StringProperty()
 
@@ -19,14 +21,16 @@ class SecondPlayerScreen(Screen):
     def append_character(self):
         self.results = self.results + choice('abc')
 
+
 class MultiscreenApp(App):
 
     def build(self):
-        # Create the screen manager
         sm = ScreenManager()
+        # Note that each screen MUST have a name.
+        # If you want to switch screens you can use sm.current = 'screen_name', but
+        #   in this simple example, Kivy code handles this.
         sm.add_widget(FirstPlayerScreen(name='First'))
         sm.add_widget(SecondPlayerScreen(name='Second'))
-
         return sm
 
 if __name__ == '__main__':
